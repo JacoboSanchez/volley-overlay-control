39190f5ce91c3106699aa6a686368ffeef0d2921
Jacobo Sánchez <jacobo.sanchez@gmail.com>
2025-09-01 22:10:49 +0100

    Switch to User fixture tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,8 @@
 import pytest
 import os
+from nicegui.testing import User
+from typing import Generator
+from app.startup import startup
 from dotenv import load_dotenv
 
 os.environ['PYTEST_CURRENT_TEST'] = 'true'
@@ -11,3 +14,9 @@ def load_test_env():
     dotenv_path = os.path.join(os.path.dirname(__file__), '..', '.env.test')
     if os.path.exists(dotenv_path):
         load_dotenv(dotenv_path=dotenv_path)
+
+        
+@pytest.fixture
+def user(user: User) -> Generator[User, None, None]:
+    startup()
+    yield user
